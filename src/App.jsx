dc222b3500d16bd8572bd80795909f8fd71bd7a4
Nikita <nekit9401@yandex.ru>
2025-02-28 00:58:07 +0300

    Final project

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,95 @@ import { useState } from 'react';
 import styles from './app.module.css';
 
 export const App = () => {
-	return <></>;
+	const [operand1, setOperand1] = useState('');
+	const [operand2, setOperand2] = useState('');
+	const [operator, setOperator] = useState('');
+	const [result, setResult] = useState('');
+	const [isResultOnDisply, setIsResultOnDisplay] = useState(false);
+
+	const buttons = ['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '=', '0', 'C'];
+
+	const calcResult = (operator) => {
+		switch (operator) {
+			case '+':
+				return Number(operand1) + Number(operand2);
+			case '-':
+				return Number(operand1) - Number(operand2);
+		}
+	};
+
+	const handleButtonClick = (value) => {
+		if (value === 'C') {
+			setOperand1('');
+			setOperand2('');
+			setOperator('');
+			setResult('');
+			setIsResultOnDisplay(false);
+		} else if (value === '+' || value === '-') {
+			if (isResultOnDisply) {
+				setOperand1(String(result));
+				setIsResultOnDisplay(false);
+				setOperator(value);
+			}
+			if (operand1 && !operator) {
+				setOperator(value);
+			}
+		} else if (value === '=') {
+			if (operand1 && operand2) {
+				setResult(calcResult(operator));
+				setIsResultOnDisplay(true);
+				setOperand1('');
+				setOperand2('');
+				setOperator('');
+			}
+		} else if (value === '0') {
+			if (operand1 && operand2) {
+				setOperand2((op1) => op1 + value);
+			} else if (operand1 && !operator) {
+				setOperand1((op1) => op1 + value);
+			}
+		} else {
+			if (isResultOnDisply) {
+				setResult('');
+				setIsResultOnDisplay(false);
+				setOperand1((op1) => op1 + value);
+			} else {
+				if (!operator) {
+					setOperand1((op1) => op1 + value);
+				}
+				if (operator) {
+					setOperand2((op2) => op2 + value);
+				}
+			}
+		}
+	};
+
+	return (
+		<>
+			<div className={styles.calculator}>
+				<div className={`${styles.display} ${isResultOnDisply ? styles.result : ''}`}>
+					{isResultOnDisply
+						? result
+						: !operand1
+							? '0'
+							: `${operand1}${operator}${operand2}`}
+				</div>
+				<div className={styles.buttons}>
+					{buttons.map((btn) => (
+						<button
+							key={btn}
+							className={`${styles.button} ${
+								btn === '+' || btn === '-' ? styles.operator : ''
+							} ${btn === '=' ? styles['operator-equally'] : ''} ${
+								btn === 'C' ? styles.clear : ''
+							}`}
+							onClick={() => handleButtonClick(btn)}
+						>
+							{btn}
+						</button>
+					))}
+				</div>
+			</div>
+		</>
+	);
 };
